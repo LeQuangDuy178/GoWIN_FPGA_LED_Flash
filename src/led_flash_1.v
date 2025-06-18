a69a2c2d158b0da7dcf74e89d48efbd4434514f6
Duy Le <82302283+LeQuangDuy178@users.noreply.github.com>
2025-06-18 23:23:24 +0700

    FPGA

---
+++ src/led_flash_1.v
@@ -0,0 +1,27 @@
+//module led_flash_1(
+//    input clk,
+//    input rst_n,
+//    output reg led
+//);
+//    reg [25:0] cnt;
+//    always @ (posedge clk or negedge rst_n)
+//    begin
+//        if (!rst_n)
+//            cnt <= 26'd0;
+//        else if (cnt < 26'd49_999_999)
+//            cnt <= cnt + 1'b1;
+//        else
+//            cnt <= 26'd0;
+//    end
+
+//    always @ (posedge clk or negedge rst_n)
+//    begin
+//        if (!rst_n)
+//            led <= 1'b0;
+//        else if (cnt == 26'd49_999_999)
+//            led <= ~led;
+//        else
+//        led <= led;
+//    end
+//    
+//endmodule
